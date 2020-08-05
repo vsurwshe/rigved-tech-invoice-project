acdfa62c9248e47441d3a51e465af463d3596a51
Vishvanath Surwshe <vishvanath.surwshe@rigvedtech.com>
2020-08-05 13:50:40 +0530

    0.8.2: Added the left side invoice tab

---
+++ src/views/invoice/InvoiceManagement.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class InvoiceManagement extends Component {
+    state = {  };
+    
+    render() {
+        return <h1>Invoice Management</h1>;
+    }
+}
+
+export default InvoiceManagement;
