f1b88e07428e0b278935623025fe4ad0141b7d4d
Vishvanath Surwshe <vishvanath.surwshe@rigvedtech.com>
2020-08-21 14:59:06 +0530

    0.14.8: Added the project and po share pie chart

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -63,24 +63,46 @@ class Dashboard extends Component {
           { field:"Client 4", value:17}
         ]
 
+        const ProjectRevenue=[
+          { field:"Project 1", value:20},
+          { field:"Project 2", value:30},
+          { field:"Project 3", value:43},
+          { field:"Project 4", value:17}
+        ]
+
+        const PurchaseOrderRevenue=[
+          { field:"PO 1", value:20},
+          { field:"PO 2", value:30},
+          { field:"PO 3", value:43},
+          { field:"PO 4", value:17}
+        ]
+
         return <>
-            <Grid container spacing={6}>
-              <Grid item xs={12} sm={6}>
-                <StackedBarChart chartData={BillingData} max={2000} series={BillingSerise} title="Billing Data" />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <StackedBarChart chartData={EmployeeData} max={2000} series={EmployeeSerise} title="Employee Data" />
-              </Grid>
-            </Grid>
-            <Grid container spacing={6}>
-              <Grid item xs={12} sm={6}>
-                <SimpleLineChart dataSource={ResourceData} architectureSources={ResourceSerise} title="Resource Data"/>
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <SimplePieChart dataSource={ClientRevenue} title="Client revenue share"/>
-              </Grid>
-            </Grid>
-        </>
+        <Grid container spacing={6}>
+          <Grid item xs={12} sm={6}>
+            <StackedBarChart dataSource={BillingData} xAxisText="Billing" title="Billing Data" series={BillingSerise}  />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <StackedBarChart dataSource={EmployeeData} xAxisText="Employees" title="Employee Data" series={EmployeeSerise}  />
+          </Grid>
+        </Grid>
+        <Grid container spacing={6}>
+          <Grid item xs={12} sm={6}>
+            <SimpleLineChart dataSource={ResourceData} xAxisText="Employees" title="Resource Data" architectureSources={ResourceSerise} title="Resource Data"/>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <SimplePieChart dataSource={ClientRevenue} title="Client revenue share"/>
+          </Grid>
+        </Grid>
+        <Grid container spacing={6}>
+          <Grid item xs={12} sm={6}>
+          <SimplePieChart dataSource={ProjectRevenue} title="Project revenue share"/>
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <SimplePieChart dataSource={PurchaseOrderRevenue} title="Purchase order share"/>
+          </Grid>
+        </Grid>
+      </>
     }
 }
 
