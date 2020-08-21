2eb10484dc6f969bb7af940f1d87618bb16fd57a
Vishvanath Surwshe <vishvanath.surwshe@rigvedtech.com>
2020-08-21 14:59:06 +0530

    0.14.8: Added the project and po share pie chart

--- src/views/dashboard/chart/SimpleLineChart.js
+++ src/views/dashboard/chart/SimpleLineChart.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import SelectBox from 'devextreme-react/select-box';
-import { Chart, Series, ArgumentAxis, CommonSeriesSettings, CommonAxisSettings, Grid, Export, Legend, Margin, Tooltip, Label, Format } from 'devextreme-react/chart';
+import { Chart, ValueAxis, Title, Series, ArgumentAxis, CommonSeriesSettings, CommonAxisSettings, Grid, Export, Legend, Margin, Tooltip, Label, Format } from 'devextreme-react/chart';
 
 const SimpleLineChart=(propsData)=>{
-  const { dataSource,architectureSources, title}=propsData
+  const { dataSource,architectureSources, title, xAxisText}=propsData
   return <Paper>
       <Chart palette="Violet" dataSource={dataSource} title={title} >
           <CommonSeriesSettings argumentField="xaxis" type="spline" />
           <CommonAxisSettings> <Grid visible={true} /> </CommonAxisSettings>
           { architectureSources.map((item,key)=>{ return <Series key={key} valueField={item.value} name={item.name} /> })}
           <Margin bottom={20} />
+          <ValueAxis position="left"> <Title text={xAxisText}/></ValueAxis>
           <ArgumentAxis allowDecimals={false} axisDivisionFactor={60} >
             <Label> <Format type="decimal" /> </Label>
           </ArgumentAxis>
-          <Legend verticalAlignment="top" horizontalAlignment="right" />
+          <Legend verticalAlignment="bottom" horizontalAlignment="center" itemTextPosition="right" />
           <Export enabled={true} />
           <Tooltip enabled={true} />
       </Chart>
--- src/views/dashboard/chart/StackedBarChart.js
+++ src/views/dashboard/chart/StackedBarChart.js
@@ -1,36 +1,20 @@
-import * as React from 'react';
+import React from 'react';
 import Paper from '@material-ui/core/Paper';
-import { Chart, ArgumentAxis, ValueAxis, BarSeries, Title, Legend } from '@devexpress/dx-react-chart-material-ui';
-import { withStyles } from '@material-ui/core/styles';
-import { Stack, Animation } from '@devexpress/dx-react-chart';
-
-const legendStyles = () => ({
-    root: {
-      display: 'flex',
-      margin: 'auto',
-      flexDirection: 'row',
-    },
-  });
-const legendRootBase = ({ classes, ...restProps }) => ( <Legend.Root {...restProps} className={classes.root} />);
-const Root = withStyles(legendStyles, { name: 'LegendRoot' })(legendRootBase);
-const legendLabelStyles = () => ({ label: { whiteSpace: 'nowrap'} });
-const legendLabelBase = ({ classes, ...restProps }) => ( <Legend.Label className={classes.label} {...restProps} />);
-const Label = withStyles(legendLabelStyles, { name: 'LegendLabel' })(legendLabelBase);
+import { Chart, Series, CommonSeriesSettings, Legend, ValueAxis, Title, Export, Tooltip } from 'devextreme-react/chart';
 
 const StackedBarChart=(propsData)=>{
-    const { chartData, title, max, series }= propsData
-    const stackSeries = (series && series.length >0) && series.map((item,key)=>item.name);
-    return <Paper>
-    <Chart data={chartData} type="stackedBar">
-      <ArgumentAxis />
-      <ValueAxis max={max} />
-        {(series && series.length  >0)&& series.map((item,key)=><BarSeries name={item.name} valueField={item.value}  argumentField="xaxis" />)}
-      <Animation />
-      <Legend position="bottom" rootComponent={Root} labelComponent={Label} />
-      <Title text={title} />
-      <Stack stacks={[ { series:stackSeries } ]}/>
+  const {dataSource,title, xAxisText, series }=propsData
+  return <Paper>
+    <Chart id={title} title={title} dataSource={dataSource} >
+        <CommonSeriesSettings argumentField="xaxis" type="stackedBar" />
+        {(series && series.length  >0)&& series.map((item,key)=><Series valueField={item.value} name={item.name} />)}
+        <ValueAxis position="left"> <Title text={xAxisText}/></ValueAxis>
+        <Legend verticalAlignment="bottom" horizontalAlignment="center" itemTextPosition="right" />
+        <Export enabled={true} />
+        <Tooltip enabled={true} location="edge" customizeTooltip={customizeTooltip} />
     </Chart>
   </Paper>
 }
+const customizeTooltip=(arg)=>{ return { text: `${arg.seriesName } = ${ arg.valueText}` }; }
 
 export default StackedBarChart;
