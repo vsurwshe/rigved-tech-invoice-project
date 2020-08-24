df49098f9fa97ed0ef966d1456830a8a89e4736a
Vishvanath Surwshe <vishvanath.surwshe@rigvedtech.com>
2020-08-24 13:33:04 +0530

    0.14.11: start with filter with client list

--- src/views/dashboard/styles.js
+++ src/views/dashboard/styles.js
@@ -112,4 +112,24 @@ export default makeStyles(theme => ({
     fontSize: "18px !important",
     marginLeft: theme.spacing(1),
   },
+  pageTitleContainer: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(5),
+  },
+  typo: {
+    color: theme.palette.text.hint,
+  },
+  button: {
+    boxShadow: theme.customShadows.widget,
+    textTransform: "none",
+    "&:active": {
+      boxShadow: theme.customShadows.widgetWide,
+    },
+  },
+ autocomplete:{
+  width:"30%", 
+  marginTop:"-25px"
+ } 
 }));
