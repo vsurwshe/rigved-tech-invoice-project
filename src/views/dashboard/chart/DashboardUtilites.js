3046358c6263ad7629d9a895ecae9aff60ac8f58
Vishvanath Surwshe <vishvanath.surwshe@rigvedtech.com>
2020-08-25 13:46:26 +0530

    0.14.13: modify the chart information and code

--- src/views/dashboard/chart/DashboardUtilites.js
+++ src/views/dashboard/chart/DashboardUtilites.js
@@ -7,30 +7,30 @@ import { TextField } from "@material-ui/core"
 import Autocomplete from '@material-ui/lab/Autocomplete';
 
 const PageTitle=(props)=>{
-  const { title, clientSerise, filterByClient }=props
+  const { title, clientSerise, filterByClient, projectSerise }=props
   const classes = useStyles();
    return <div className={classes.pageTitleContainer}>
             <Typography className={classes.typo} variant="h3" size="sm"> {title} </Typography>
-             <Autocomplete
-                 id="client-serach"
-                 autoHighlight
-                 options={(clientSerise && clientSerise.length >0) ? clientSerise: []}
-                 getOptionLabel={clientSerise => (clientSerise && clientSerise.name) && clientSerise.name}
-                 getOptionSelected={(option, value) => option.name === value.name}
-                 onChange={(event, value) =>filterByClient(value)}
-                 className={classes.autocomplete}
-                 renderInput={(params) => ( <TextField {...params} label="Serach By Client" /> )}
-              />
-              {/* <Autocomplete
+                <Autocomplete   
+                    id="client-serach"
+                    autoHighlight
+                    options={(clientSerise && clientSerise.length >0) ? clientSerise: []}
+                    getOptionLabel={clientSerise => (clientSerise && clientSerise.name) && clientSerise.name}
+                    getOptionSelected={(option, value) => option.name === value.name}
+                    onChange={(event, value) =>filterByClient(value)}
+                    className={classes.autocomplete}
+                    renderInput={(params) => ( <TextField {...params} label="Serach By Client" /> )}
+                />
+                <Autocomplete
                     id="project-serach"
                     autoHighlight
-                    options={(clientOptionData && clientOptionData.length >0) ? clientOptionData: []}
-                    getOptionLabel={clientOptionData => (clientOptionData && clientOptionData.title) && clientOptionData.title}
-                    getOptionSelected={(option, value) => option.id === value.id}
+                    options={(projectSerise && projectSerise.length >0) ? projectSerise: []}
+                    getOptionLabel={projectSerise => (projectSerise && projectSerise.name) && projectSerise.name}
+                    getOptionSelected={(option, value) => option.name === value.name}
                     onChange={(event, value) =>console.log("Value ", value)}
                     className={classes.autocomplete}
                     renderInput={(params) => ( <TextField {...params} label="Serach By project" /> )}
-              /> */}
+                />
           </div>
   }
 
