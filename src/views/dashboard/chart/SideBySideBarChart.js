4edb2c6458f7f1cae5ae82a2ded38237a053354f
Vishvanath Surwshe <vishvanath.surwshe@rigvedtech.com>
2020-08-27 09:03:50 +0530

    0.14.16: Modify the side bar

--- src/views/dashboard/chart/SideBySideBarChart.js
+++ src/views/dashboard/chart/SideBySideBarChart.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
-import { Chart, ValueAxis, Title, Series, AdaptiveLayout, CommonSeriesSettings, Legend, Export, Tooltip } from 'devextreme-react/chart';
+import { Chart, ValueAxis, Title, Series, CommonSeriesSettings, Legend, Export, Tooltip } from 'devextreme-react/chart';
+
+const palette =[
+    '#00bcd4', 
+    '#DD2C00', 
+    '#75B5D6', 
+    '#B78C9B', 
+    '#F2CA84', 
+    '#A7CA74'
+]
 
 const SideBySideBarChart=(propsData)=>{
     const  { dataSource, title, architectureSources, xAxisText}= propsData
     return <Paper>  
-        <Chart id="chart" title={title} dataSource={dataSource} onPointClick={onPointClick}   >
+        <Chart id="chart" palette={palette} title={title} dataSource={dataSource} onPointClick={onPointClick}   >
             <CommonSeriesSettings argumentField="xaxis" type="bar" hoverMode="allArgumentPoints" selectionMode="allArgumentPoints" />
              {(architectureSources && architectureSources.length >0) && architectureSources.map((item,key)=> <Series key={key} valueField={item.value} name={item.name} />)}
             <ValueAxis position="left"> <Title text={xAxisText}/></ValueAxis>
