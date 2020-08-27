cb8706d243bce64b8b1b18d3215cd25c0d4db434
Vishvanath Surwshe <vishvanath.surwshe@rigvedtech.com>
2020-08-27 14:04:56 +0530

    0.14.19: Modify the dashboard code

--- src/redux/reducer/DashboardState.js
+++ src/redux/reducer/DashboardState.js
@@ -1,15 +1,65 @@
 const initialState = {
-    clientSerise: [],
+    clientSerise: [
+        {name:"Client 1", value:"client1"},
+        {name:"Client 2", value:"client2"},
+        {name:"Client 3", value:"client3"},
+        {name:"Client 4", value:"client4"}
+    ],
     projectSerise: [],
-    billSerise:[],
-    resourceSerise:[],
-    clientBillingData: [],
-    clientEmployeeData:[],
-    billedData:[],
-    resourceData:[],
-    clientRevenueData:[],
-    projectRevenueData:[],
-    purchaseOrderRevenueData:[]
+    billSerise:[
+        {name:"Billed", value:"billed"},
+        {name:"Revenue", value:"unbilled"}
+    ],
+    resourceSerise:[
+        {name:"Hired", value:"hired"},
+        {name:"Left", value:"left"}
+    ],
+    clientBillingData: [
+        { xaxis: 'Feb', client1:74.2,  client2: 308.6,  client3: 35.1,  client4: 956.9},
+        { xaxis: 'March', client1: 40,   client2: 128.5, client3: 361.8, client4: 105 },
+        { xaxis: 'April', client1: 22.6,  client2: 241.5,  client3: 64.9,  client4: 120.8}, 
+        { xaxis: 'May', client1: 19,  client2: 119.3,  client3: 28.9,  client4: 204.8}, 
+        { xaxis: 'Jun', client1: 6.1,  client2: 123.6, client3: 77.3, client4: 85.7 }
+    ],
+    clientEmployeeData:[
+        { xaxis: 'Feb', client1:10,  client2: 30,  client3: 20,  client4: 10},
+        { xaxis: 'March', client1: 20,   client2: 12, client3: 20, client4: 15 },
+        { xaxis: 'April', client1: 22,  client2: 24,  client3: 10,  client4: 12}, 
+        { xaxis: 'May', client1: 15,  client2: 11,  client3: 28,  client4: 20}, 
+        { xaxis: 'Jun', client1: 6.1,  client2: 12, client3: 20, client4: 15 }
+    ],
+    billedData:[
+        { xaxis: 'Feb', billed:1000,  unbilled: 3000 },
+        { xaxis: 'March', billed: 2000, unbilled: 1200 },
+        { xaxis: 'April', billed: 2200, unbilled: 2400 }, 
+        { xaxis: 'May', billed: 1500, unbilled: 1100 }, 
+        { xaxis: 'Jun', billed: 6300, unbilled: 1200 }
+    ],
+    resourceData:[
+        { xaxis: 'Feb', hired:10,  left: 30 },
+        { xaxis: 'March', hired: 20,   left: 12 },
+        { xaxis: 'April', hired: 22,  left: 24 }, 
+        { xaxis: 'May', hired: 15,  left: 11 }, 
+        { xaxis: 'Jun', hired: 6.1,  left: 12 }
+    ],
+    clientRevenueData:[
+        { field:"Client 1", value:20},
+        { field:"Client 2", value:30},
+        { field:"Client 3", value:43},
+        { field:"Client 4", value:17}
+    ],
+    projectRevenueData:[
+        { field:"Project 1", value:20, name:"Project 1"},
+        { field:"Project 2", value:30, name:"Project 2"},
+        { field:"Project 3", value:43, name:"Project 3"},
+        { field:"Project 4", value:17, name:"Project 4"}
+    ],
+    purchaseOrderRevenueData:[
+        { field:"PO 1", value:20},
+        { field:"PO 2", value:30},
+        { field:"PO 3", value:43},
+        { field:"PO 4", value:17}
+    ]
 }
 
 const DashboardState=(state = initialState, action)=>{
--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -9,76 +9,6 @@ import * as DashboardAction from "../../redux/actions/DashboardAction";
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-const ClientSerise=[
-  {name:"Client 1", value:"client1"},
-  {name:"Client 2", value:"client2"},
-  {name:"Client 3", value:"client3"},
-  {name:"Client 4", value:"client4"}
-]
-
-const TempBillingData=[
-    { xaxis: 'Feb', client1:74.2,  client2: 308.6,  client3: 35.1,  client4: 956.9},
-    { xaxis: 'March', client1: 40,   client2: 128.5, client3: 361.8, client4: 105 },
-    { xaxis: 'April', client1: 22.6,  client2: 241.5,  client3: 64.9,  client4: 120.8}, 
-    { xaxis: 'May', client1: 19,  client2: 119.3,  client3: 28.9,  client4: 204.8}, 
-    { xaxis: 'Jun', client1: 6.1,  client2: 123.6, client3: 77.3, client4: 85.7 }
-];
-
-const TempEmployeeData=[
-    { xaxis: 'Feb', client1:10,  client2: 30,  client3: 20,  client4: 10},
-    { xaxis: 'March', client1: 20,   client2: 12, client3: 20, client4: 15 },
-    { xaxis: 'April', client1: 22,  client2: 24,  client3: 10,  client4: 12}, 
-    { xaxis: 'May', client1: 15,  client2: 11,  client3: 28,  client4: 20}, 
-    { xaxis: 'Jun', client1: 6.1,  client2: 12, client3: 20, client4: 15 }
-];
-
-const ResourceSerise=[
-  {name:"Hired", value:"hired"},
-  {name:"Left", value:"left"}
-]
-
-const ResourceData=[
-  { xaxis: 'Feb', hired:10,  left: 30 },
-  { xaxis: 'March', hired: 20,   left: 12 },
-  { xaxis: 'April', hired: 22,  left: 24 }, 
-  { xaxis: 'May', hired: 15,  left: 11 }, 
-  { xaxis: 'Jun', hired: 6.1,  left: 12 }
-]
-
-const BillSerise=[
-  {name:"Billed", value:"billed"},
-  {name:"Revenue", value:"unbilled"}
-]
-
-const BilledData=[
-  { xaxis: 'Feb', billed:1000,  unbilled: 3000 },
-  { xaxis: 'March', billed: 2000, unbilled: 1200 },
-  { xaxis: 'April', billed: 2200, unbilled: 2400 }, 
-  { xaxis: 'May', billed: 1500, unbilled: 1100 }, 
-  { xaxis: 'Jun', billed: 6300, unbilled: 1200 }
-]
-
-const ClientRevenue=[
-  { field:"Client 1", value:20},
-  { field:"Client 2", value:30},
-  { field:"Client 3", value:43},
-  { field:"Client 4", value:17}
-]
-
-const ProjectRevenue=[
-  { field:"Project 1", value:20, name:"Project 1"},
-  { field:"Project 2", value:30, name:"Project 2"},
-  { field:"Project 3", value:43, name:"Project 3"},
-  { field:"Project 4", value:17, name:"Project 4"}
-]
-
-const PurchaseOrderRevenue=[
-  { field:"PO 1", value:20},
-  { field:"PO 2", value:30},
-  { field:"PO 3", value:43},
-  { field:"PO 4", value:17}
-]
-
 class Dashboard extends Component {
   state = { 
     BillingData : [], 
@@ -87,21 +17,23 @@ class Dashboard extends Component {
   }
 
   componentDidMount=async()=>{
-    console.log("Props ",this.props)
-    const { clientBillingData, clientEmployeeData }=this.props.DashboardState
+    const { clientBillingData, clientEmployeeData, projectRevenueData }=this.props.DashboardState
     const { authorization }=this.props.LoginState
-    const {GetBillingData }=this.props.DashboardAction
+    const { GetBillingData }=this.props.DashboardAction
     if((clientBillingData && clientBillingData.length <=0) || (clientEmployeeData && clientEmployeeData.length <=0))
-    {
-      await GetBillingData(authorization);
-    }
-
+    { await GetBillingData(authorization); }
+    await this.handelBillingData(clientBillingData)
+    await this.handelEmployeeData(clientEmployeeData)
+    await this.handelProjectData(projectRevenueData)
   }
 
+  // this method will handel the state billing data
   handelBillingData=(BillingData)=>{this.setState({BillingData})}
   
+  // this method will handel the state employee data
   handelEmployeeData=(EmployeeData)=>{this.setState({EmployeeData})}
   
+  // this method will handel the state project data
   handelProjectData=(ProjectData)=>{this.setState({ProjectData})}
   
   render() { 
@@ -126,6 +58,7 @@ class Dashboard extends Component {
     </>
   }
 
+  // this method will load the page title
   loadPageTitle=()=>{
     const { clientSerise, projectSerise }= this.props.DashboardState
     return <PageTitle title="Dashboard" 
@@ -136,16 +69,19 @@ class Dashboard extends Component {
     />
   }
 
+  // this method will load the client billing chart 
   loadClientBillingChart=()=>{
-    const { clientBillingData, clientSerise }= this.props.DashboardState
+    const { clientSerise }= this.props.DashboardState
+    const { EmployeeData }= this.state
     return <StackedBarChart 
-        dataSource={clientBillingData} 
+        dataSource={EmployeeData} 
         architectureSources={clientSerise} 
         xAxisText="Billing" 
         title="Client Billing Data"   
     /> 
   }
 
+  // this method will load the billing chart
   loadBillingChart=()=>{
     const { billedData, billSerise }= this.props.DashboardState
     return <SideBySideBarChart 
@@ -156,16 +92,19 @@ class Dashboard extends Component {
     />
   }
 
+  // this method will load the employee chart
   loadEmployeeChart=()=>{
-    const { clientEmployeeData, clientSerise }=this.props.DashboardState
+    const { clientSerise }=this.props.DashboardState
+    const { EmployeeData }=this.state
     return <StackedBarChart 
-        dataSource={clientEmployeeData} 
+        dataSource={EmployeeData} 
         architectureSources={clientSerise}  
         xAxisText="Employees" 
         title="Employee Data" 
     />
   }
 
+  // this method will load the resource chart
   loadResourceChart=()=>{
     const { resourceData, resourceSerise }=this.props.DashboardState
     return <SimpleLineChart 
@@ -176,6 +115,7 @@ class Dashboard extends Component {
     />
   }
 
+  // this method will load the client revenue chart
   loadClientRevenueChart=()=>{
     const {clientRevenueData}=this.props.DashboardState
     return <SimplePieChart 
@@ -184,14 +124,16 @@ class Dashboard extends Component {
     />
   }
 
+  // this method will load the project chart
   loadProjectRevenueChart=()=>{
-    const { projectRevenueData }=this.props.DashboardState
+    const { ProjectData }=this.state
     return <SimplePieChart 
-      dataSource={projectRevenueData} 
+      dataSource={ProjectData} 
       title="Project revenue share"
     />
   }
 
+  // this method will load the purchase order revenue chart
   loadPurchaseOrderRevenueChart=()=>{
     const { purchaseOrderRevenueData }= this.props.DashboardState
     return <SimplePieChart 
@@ -200,27 +142,31 @@ class Dashboard extends Component {
     />
   }
 
+  // this method will filter by client
   filterByClient=(propsData)=>{
+    const { clientBillingData, clientEmployeeData }=this.props.DashboardState
     if(propsData){
       let keyValue=propsData.value;
-      let tempBillingFilter = (TempBillingData.length >0) && TempBillingData.filter((item)=>item.hasOwnProperty(propsData.value))
+      let tempBillingFilter = (clientBillingData.length >0) && clientBillingData.filter((item)=>item.hasOwnProperty(propsData.value))
       .map((item)=> {return {"xaxis":item.xaxis , [keyValue] : item.[keyValue]}});
-      let tempEmployeeFilter = (TempEmployeeData.length >0) && TempEmployeeData.filter((item)=>item.hasOwnProperty(propsData.value))
+      let tempEmployeeFilter = (clientEmployeeData.length >0) && clientEmployeeData.filter((item)=>item.hasOwnProperty(propsData.value))
       .map((item)=> {return {"xaxis":item.xaxis , [keyValue] : item.[keyValue]}});
       this.handelBillingData(tempBillingFilter);
       this.handelEmployeeData(tempEmployeeFilter);
     }else{
-      this.handelBillingData(TempBillingData);
-      this.handelEmployeeData(TempEmployeeData);
+      this.handelBillingData(clientBillingData);
+      this.handelEmployeeData(clientEmployeeData);
     }
   }
 
+  // this method will filter by project
   filterByProject=(propsData)=>{
+    const { projectRevenueData }=this.props.DashboardState
     if(propsData){
-      let tempProjectFilter = (ProjectRevenue.length >0) && ProjectRevenue.filter((item)=>item.field === propsData.field);
+      let tempProjectFilter = (projectRevenueData && projectRevenueData.length >0) && projectRevenueData.filter((item)=>item.field === propsData.field);
       this.handelProjectData(tempProjectFilter);
     }else{
-      this.handelProjectData(ProjectRevenue);
+      this.handelProjectData(projectRevenueData);
     }
   }
 }
